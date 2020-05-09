a931653cb4d24948cfc2f27f013a76698d8ebc18
Volodymyr Tytenko <tvolodi@hotmail.com>
2020-05-09 16:32:06 +0600

    Some tests with UNO are finished

---
+++ GammaWorksClient/GammaWorksClient.Shared/Model/BaseTask.cs
@@ -0,0 +1,27 @@
+﻿using Realms;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GammaWorksClient.Shared.Model
+{
+    public class BaseTask : RealmObject
+    {
+        [PrimaryKey]
+        public string Id { get; set; } = Guid.NewGuid().ToString(); // Unique object id
+
+        public long RegNo { get; set; } = 0; // Centerlized registered number
+
+        public string Descr { get; set; } = "";
+
+        public DateTimeOffset PlannedStart { get; set; } = DateTimeOffset.MinValue;
+        public DateTimeOffset PlannedFinish { get; set; } = DateTimeOffset.MinValue;
+        public DateTimeOffset ActualStart { get; set; } = DateTimeOffset.MinValue;
+        public DateTimeOffset ActualFinish { get; set; } = DateTimeOffset.MinValue;
+
+        public int Priority { get; set; } = 5;
+
+
+        
+    }
+}
---
+++ GammaWorksClient/GammaWorksClient.Shared/ViewModel/BaseTaskVM.cs
@@ -0,0 +1,23 @@
+﻿using GammaWorksClient.Shared.Model;
+using Realms;
+using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace GammaWorksClient.Shared.ViewModel
+{
+    public class BaseTaskVM
+    {
+        Realm realmInstance;
+
+        public BaseTaskVM()
+        {
+            realmInstance = Realm.GetInstance();
+        }
+
+    }
+}
